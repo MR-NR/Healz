57fcdac83f91eea7e237969aacd7eabaef6d5d44
Aamir <aamirrajpot6@gmail.com>
2019-09-18 11:21:51 +0500

    About Page Integrated
    About Page Integration Done with a bug fixed(Logo link was not working its fixed now1)

--- Healz/Healz/Controllers/HomeController.cs
+++ Healz/Healz/Controllers/HomeController.cs
@@ -14,6 +14,10 @@ namespace Healz.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
 
         public IActionResult Privacy()
         {

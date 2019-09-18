4d79ae6ddb45b6104fa6cf17df1c753c417acf52
bscs351 <rfatima590>
2019-09-18 12:17:06 +0500

    Index Page
    1st Index Page Complete

--- Healz/Healz/Controllers/HomeController.cs
+++ Healz/Healz/Controllers/HomeController.cs
@@ -14,11 +14,12 @@ namespace Healz.Controllers
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        public IActionResult searchresultv1()
         {
             return View();
         }
+
+       
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
